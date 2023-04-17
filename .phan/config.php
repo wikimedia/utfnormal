6f3690d3ca446d9745fa3410bb993470afd7bc25
James D. Forrester <jforrester@wikimedia.org>
2023-04-17 16:37:19 -0400

    build: Switch phan to special library mode
    Bug: T334492
    Change-Id: I642a6f6a56c1ad86d45d6369c4126ddc4970202b

--- .phan/config.php
+++ .phan/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
 
 // T303790 - Can be removed when extension doesn't need to support < PHP 8.1
 $cfg['suppress_issue_types'][] = 'PhanImpossibleTypeComparison';
