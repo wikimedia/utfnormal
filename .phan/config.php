b45b5fb612993a4421fd0950ae0cd92f85708a7f
Umherirrender <umherirrender_de.wp@web.de>
2025-07-26 17:04:51 +0200

    Remove compat code about normalizer_normalize
    Bug: T303790
    Change-Id: I27a8ca5b360c4e6f0b5634cdbe037cb0f089700f

--- .phan/config.php
+++ .phan/config.php
@@ -1,8 +1,3 @@
 <?php
 
-$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
-
-// T303790 - Can be removed when extension doesn't need to support < PHP 8.1
-$cfg['suppress_issue_types'][] = 'PhanImpossibleTypeComparison';
-
-return $cfg;
+return require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config-library.php';
